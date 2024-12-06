6183c499d34a33f8c5772b5e38efb13867f2debd
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-12-06 23:14:04 +0800

    testing fib, unsure if its correct

--- fib.cpp
+++ fib.cpp
@@ -8,7 +8,7 @@ int fib (int n) {
     if (n <= 1) {
         return n;  
     }
-    return fib(n - 1) + fib(n - 2);  // Recursive case meant for Fibonacci
+    return fib(n - 1) + fib(n - 2);  // Recursive case meant for Fibonacci,i think - its a work in progress
 }
 
 int main() {
