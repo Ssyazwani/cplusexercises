a4c595fdca21326110d716912a44c482d408ce8d
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-12-06 15:56:59 +0800

    inputted calculator code (one way)

--- calcutor.cpp
+++ calcutor.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
 
-int main(){
+int main() {
 
+  char input;
+  float num1, num2;
 
+  cout << "Enter operator: +, -, *, /: ";
+  cin >> input;
+
+  cout << "Enter two operands: ";
+  cin >> num1 >> num2;
+
+  switch(input) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      
+      cout << "Invalid Input";
+      break;
+  }
+
+  return 0;
 }
