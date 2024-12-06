3955e8451f20e108e6e6de03dd5bb97a37490cd1
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-12-06 18:06:56 +0800

    edited the code for recursive

--- fib.cpp
+++ fib.cpp
@@ -1,1 +1,26 @@
 //Write a recursive function that finds the #n integer of the Fibonacci sequence. Then build a minimal program to test it.
+
+#include <iostream>
+using namespace std;
+
+
+int fib (int n) {
+    if (n <= 1) {
+        return n;  
+    }
+    return fib(n - 1) + fib(n - 2);  // Recursive case meant for Fibonacci
+}
+
+int main() {
+    int x;
+
+    cout << "Enter a number to find its Fibonacci value: ";
+    cin >> x;
+
+    int output = fib(x);
+
+    cout << "Fibonacci number at position " << x << " is: " << output << endl;
+
+    return 0;
+}
+
