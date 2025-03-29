09cbe087d754e45aa8da961e6e56b3026533dc52
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-03-29 22:26:05 +0800

    editing testinglist to add actor full name to database

--- testinglist.cpp
+++ testinglist.cpp
@@ -1,41 +1,46 @@
+
 #include <iostream>
 #include <list>
+#include <string>
 using namespace std;
 
-int x;
-string y;
+int main() {
 
-int main()
-{
-	cout << " The list of jactors recorded :\n";
-	list<string> actors = { "Takumi Kitamura", "Mio Imada", "Ren Meguro", "Nagano Mei" };
-
-	
-	for (string jactors : actors) {
-		cout << jactors << "\n";
-	}
-
-	cout << "To add in one more actor, please press 1:";
-
-	cin >> x;
+    cout << "The list of actors recorded:\n";
+    list<string> actors = { "Takumi Kitamura", "Mio Imada", "Ren Meguro", "Nagano Mei" };
 
 
-	switch (x)
-
-		case 1: 
-			cout << "\n Key in name of actor : ";
-			cin >> y;
-			actors.push_back(y);
-
-			cout << "\n The list of jators recorded :";
-
-	
-
-			for (string jactors1 : actors){
-				cout << jactors1 << "\n";
-			}
+    for (const string& jactors : actors) {
+        cout << jactors << "\n";
+    }
 
 
+    cout << "To add one more actor, please press 1: ";
 
+    int x;
+    cin >> x;
+    cin.ignore();
+    string y;
 
+    switch (x) {
+    case 1:
+        cout << "\nKey in the name of the actor: ";
+        getline(cin, y);  
+
+        actors.push_back(y);  
+
+      
+        cout << "\nThe list of actors recorded:\n";
+        for (const string& jactors1 : actors) {
+            cout << jactors1 << "\n";
+        }
+        break;
+
+    default:
+        cout << "Invalid option selected.\n";
+        break;
+    }
+
+    return 0;
 }
+
